1fc453f1c897f51d2593a4e3a6078c2c1fb4b9ea
yuqb <930174189@qq.com>
2020-04-10 12:14:06 +0800

    use react-router

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Practice from './components/practice/practice'
+import Home from './components/home/home'
 
+/**
+ * 傻逼文档 要安装也不写
+ */
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className="wrap">
-        我是容器
-      </div>
-    </div>
+    <>
+      
+      <Router>
+      <ul>
+        <li>
+          <Link to="/">App</Link>
+        </li>
+        <li>
+          <Link to="/practice">Practice</Link>
+        </li>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+      </ul>
+        {/* <Route exact path="/" component={App}> */}
+          <Route path="/home" component={Home}></Route>
+          <Route path="/practice" component={Practice}></Route>
+        {/* </Route> */}
+      </Router>
+      {/* {this.props.children} */}
+    </>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/home/home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function Home(props) {
+  return(
+    <div className="home">
+      我是Home
+    </div>
+  )
+}
